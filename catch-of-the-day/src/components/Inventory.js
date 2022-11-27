d22683dc3839f7162e09725d686fa7b45f16b728
Daniel Bastos <dbastos@consumeraffairs.com>
2022-11-27 09:02:45 +0000

    create some components and start to work with routes

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render () {
+    return (
+      <div className="inventory">
+        I'm the Inventory
+      </div>
+    )
+  }
+}
+
+export default Inventory;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Order extends React.Component {
+  render () {
+    return (
+      <div className="order">
+        I'm the Order
+      </div>
+    )
+  }
+}
+
+export default Order;
