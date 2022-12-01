565e402e2f163581ff13171b65df9c34f9a3145e
Daniel Bastos <dbastos@consumeraffairs.com>
2022-12-01 08:49:10 +0000

    create the inventory and start to implement load smaple fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,14 +5,29 @@ import Order from "./Order";
 
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    console.log('loadSampleFishes');
+  };
+
   render () {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
