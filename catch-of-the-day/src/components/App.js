835c9dcf5eb9d99ef2999b3bc4e3bd8a344b41ae
Daniel Bastos <dbastos@consumeraffairs.com>
2022-12-07 13:09:17 +0000

    fish the order component and start to use the firebase

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,24 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 import Fish from "./Fish";
 import samlpesFishes from "../sample-fishes";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const storeId = this.props.match.params.storeId;
+    this.ref = base.syncState(`${storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     const fishes = { ...this.state.fishes };
@@ -38,7 +50,7 @@ class App extends React.Component {
             )}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes} />
